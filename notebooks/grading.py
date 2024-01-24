39be5d6cc8594da19172631fd9369b9e1bab7e9e
ClaraGard <74459674+ClaraGard@users.noreply.github.com>
2024-01-24 17:30:38 +0100

    Update grading.py

--- notebooks/grading.py
+++ notebooks/grading.py
@@ -7,8 +7,11 @@ en_fr_ds = load_dataset('news_commentary', 'en-fr', split='train')
 
 en_fr_ds = pd.DataFrame(en_fr_ds['translation'])
 
-en_fr_ds = en_fr_ds[en_fr_ds["en"].str.split().str.len() > 10]
-en_fr_ds = en_fr_ds[en_fr_ds["fr"].str.split().str.len() > 10]
+en_fr_ds = en_fr_ds[en_fr_ds["en"].str.split().str.len() > 7]
+en_fr_ds = en_fr_ds[en_fr_ds["en"].str.split().str.len() < 20]
+en_fr_ds = en_fr_ds[en_fr_ds["fr"].str.split().str.len() > 7]
+en_fr_ds = en_fr_ds[en_fr_ds["fr"].str.split().str.len() < 20]
+
 
 en_fr_ds = en_fr_ds.sample(n=300, replace=False, random_state=42)
 
@@ -22,6 +25,4 @@ for index, row in en_fr_ds.iterrows():
     print("translation:", row["en"])
     print("input grade: ")
     en_fr_ds[grader][index] = int(input())
-
-
-en_fr_ds.to_csv("en-fr_translations.csv")
+    en_fr_ds.to_csv("en-fr_translations.csv")
